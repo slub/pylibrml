496f75af71a457128bfd3db2d2ad6f905927bbbf
Thomas Baer <thomas.baer@slub-dresden.de>
2021-01-11 10:52:33 +0100

    First PyPI release

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="pylibrml-pkg-slub.dd",
+    name="pylibrml",
     version="0.2.0.dev1",
     author="Thomas Baer",
     author_email="thomas.baer@slub-dresden.de",
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/slub/pylibrml",
-    packages=setuptools.find_packages(include=['common', 'model']),
+    packages=setuptools.find_packages(include=['common', 'model', 'tmpl']),
     classifiers=[
         "Development Status :: 1 - Planning",
         "Environment :: Console",
@@ -24,4 +24,5 @@ setuptools.setup(
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
     python_requires='~=3.8',
+    install_requires=['jinja2'],
 )
